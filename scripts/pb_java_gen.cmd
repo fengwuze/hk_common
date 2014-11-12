ab9d9fdee54b5e9a5029f8f5a6e4ebb88d6d232e
sunnykaka <sunnykaka0721@gmail.com>
2014-11-12 19:45:41 +0800

    init

---
+++ scripts/pb_java_gen.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+rem 运行之前请将protoc.exe所在路径加入PATH
+
+set SRC_BASEDIR=C:/Users/Liub/workspace/hk_common
+set OUT_BASEDIR=C:/Users/Liub/workspace/hk_gameserver
+
+set SRC_PATH=%SRC_BASEDIR%/src/com/origingame
+set OUT_PATH=%OUT_BASEDIR%/src/main/java
+
+protoc --proto_path=%SRC_PATH% --java_out=%OUT_PATH% %SRC_PATH%/message/*.proto
