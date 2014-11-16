c099232ec5264f401c12b783020550e2c1ca2a5e
sunnykaka <sunnykaka0721@gmail.com>
2014-11-16 20:42:53 +0800

    init

--- scripts/pb_java_gen.sh
+++ scripts/pb_java_gen.sh
@@ -2,8 +2,10 @@
 
 # 运行之前请将protoc所在路径加入PATH
 
-BASEDIR=~/devel/github/rocket
-SRC_PATH=$BASEDIR/src/main/java/com/rpg
-OUT_PATH=$BASEDIR/src/main/java
+SRC_BASEDIR=~/work/workspace/hk_common
+OUT_BASEDIR=~/work/workspace/hk_gameserver
 
-protoc --proto_path=$SRC_PATH --java_out=$OUT_PATH $SRC_PATH/rocket/domain/pb/*.proto $SRC_PATH/rocket/message/pb/*.proto
+SRC_PATH=$SRC_BASEDIR/src/com/origingame
+OUT_PATH=$OUT_BASEDIR/src/main/java
+
+protoc --proto_path=$SRC_PATH --java_out=$OUT_PATH $SRC_PATH/message/*.proto
